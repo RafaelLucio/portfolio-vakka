3caa2094879cab7ff5cdeabe6c40b0542a3abc6a
rafaellucio <rafaelantoniolucio@gmail.com>
2014-04-07 01:02:03 -0300

    layout fixed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+var heightScreen = $(window).height();
+
+$(window).scroll(function () {
+	if ($(this).scrollTop() > (heightScreen/2)) {
+		$('.logo').addClass('active');
+	}
+	if ($(this).scrollTop() < ((heightScreen/2)+(heightScreen/3))) {
+		$('.logo').removeClass('active');
+	}
+});
