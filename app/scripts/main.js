aa8c9c0822b99dff4c58c37229acaeb75c7be858
rafaellucio <rafaelantoniolucio@gmail.com>
2014-04-29 01:49:27 -0300

    include fonts josefinslab

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,22 +16,33 @@ $(function () {
 			$('.vakka').addClass('active');
 			$('.grama').addClass('active');
 			$('.placa').addClass('active');
+			$('.luz-top').addClass('active');
 			$('.home-text').addClass('active');
 			$('.arrow-bottom').addClass('active');
+			$('.content-moscas').addClass('active');
 		}
 		if ($(this).scrollTop() < 20) {
 			$('.luz').removeClass('active');
 			$('.vakka').removeClass('active');
 			$('.grama').removeClass('active');
 			$('.placa').removeClass('active');
+			$('.luz-top').removeClass('active');
 			$('.home-text').removeClass('active');
 			$('.arrow-bottom').removeClass('active');
+			$('.content-moscas').removeClass('active');
 		}
-		if ($(this).scrollTop() > heightScreen-100) {
+		if ($(this).scrollTop() > heightScreen) {
+			$('.luz-top').removeClass('active');
+		}
+		if ($(this).scrollTop() > heightScreen-35) {
 			$('.logo').addClass('active');
 		}
-		if ($(this).scrollTop() < heightScreen-100) {
+		if ($(this).scrollTop() < heightScreen-35) {
 			$('.logo').removeClass('active');
 		}
+	});
+
+	$('.navigation').on('click', function() {
+		$(this).children('a').eq(0).toggleClass('active');
 	});
 });
