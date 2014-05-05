7a547b29e3e2f8eb50992f7d4d0d891d31899fde
rafaellucio <rafaelantoniolucio@gmail.com>
2014-05-05 00:26:17 -0300

    fixed layout of breakpoints

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,9 @@
+/**
+ *    Portfolio Vakka
+ *
+ *    @author Rafael Antonio Lucio <rafaelantoniolucio@gmail.com>
+ */
+
 $(function () {
 	'use strict';
 
@@ -9,45 +15,47 @@ $(function () {
 	});
 
 	$(window).scroll(function () {
-		if ($(this).scrollTop() > 0) {
-			$('.luz').addClass('active');
-			$('.luz-top').addClass('active');
+		if ($(window).width() > 480) {
+			if ($(this).scrollTop() > 0) {
+				$('.luz').addClass('active');
+				$('.luz-top').addClass('active');
 
-			if ($(window).width() > 480) {
 				$('.vakka').children('img').attr('src', 'images/blur_vaca.png');
+				$('.placa').children('img').attr('src', 'images/blur_placa.png');
 				$('.grama').children('img').attr('src', 'images/blur_grama.png');
 			}
-		}
-		if ($(this).scrollTop() > 20) {
-			$('.vakka').addClass('active');
-			$('.grama').addClass('active');
-			$('.placa').addClass('active');
-			$('.home-text').addClass('active');
-			$('.arrow-bottom').addClass('active');
-			$('.content-moscas').addClass('active');
-		}
-		if ($(this).scrollTop() < 20) {
-			if ($(window).width() > 480) {
+			if ($(this).scrollTop() > 20) {
+				$('.vakka').addClass('active');
+				$('.grama').addClass('active');
+				$('.placa').addClass('active');
+				$('.home-text').addClass('active');
+				$('.placa-sombra').addClass('active');
+				$('.arrow-bottom').addClass('active');
+				$('.content-moscas').addClass('active');
+			}
+			if ($(this).scrollTop() < 20) {
 				$('.vakka').children('img').attr('src', 'images/vakka.png');
 				$('.grama').children('img').attr('src', 'images/grama.png');
-			}
+				$('.placa').children('img').attr('src', 'images/placa.png');
 
-			$('.luz').removeClass('active');
-			$('.vakka').removeClass('active');
-			$('.grama').removeClass('active');
-			$('.placa').removeClass('active');
-			$('.luz-top').removeClass('active');
-			$('.home-text').removeClass('active');
-			$('.arrow-bottom').removeClass('active');
-			$('.content-moscas').removeClass('active');
+				$('.luz').removeClass('active');
+				$('.vakka').removeClass('active');
+				$('.grama').removeClass('active');
+				$('.placa').removeClass('active');
+				$('.luz-top').removeClass('active');
+				$('.home-text').removeClass('active');
+				$('.placa-sombra').removeClass('active');
+				$('.arrow-bottom').removeClass('active');
+				$('.content-moscas').removeClass('active');
+			}
+			if ($(this).scrollTop() > heightScreen) {
+				$('.luz-top').removeClass('active');
+			}
 		}
-		if ($(this).scrollTop() > heightScreen) {
-			$('.luz-top').removeClass('active');
-		}
-		if ($(this).scrollTop() > heightScreen-35) {
+		if ($(this).scrollTop() > heightScreen-50) {
 			$('.logo').addClass('active');
 		}
-		if ($(this).scrollTop() < heightScreen-35) {
+		if ($(this).scrollTop() < heightScreen-50) {
 			$('.logo').removeClass('active');
 		}
 	});
