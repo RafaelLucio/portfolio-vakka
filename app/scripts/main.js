2ad3b90822a37112bfa55680119caf243b2f715d
rafaellucio <rafaelantoniolucio@gmail.com>
2014-05-26 23:52:41 -0300

    fixed display itens from home animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,9 +10,6 @@ $(function () {
 		userAgent = navigator.userAgent.match('Chrome'),
 		$homeId = $('#home'),
 		$logo = $('.logo'),
-		$luz = $('.luz'),
-		$luzTop = $('.luz-top'),
-		$placaSombra = $('.placa-sombra'),
 		$navigation = $('.navigation'),
 		elemPos = $('#about').position().top;
 
@@ -22,9 +19,7 @@ $(function () {
 
 	$(window).stellar({
 		verticalScrolling: $homeId
-	});
-
-	$(window).scroll(function () {
+	}).scroll(function () {
 		var scrollPos = $(document).scrollTop();
 
 		if (scrollPos >= (elemPos - 50)) {
@@ -33,21 +28,6 @@ $(function () {
 			$navigation.find('a').eq(0).removeClass('active');
 		}
 
-		if ($(window).width() > 480) {
-			if ($(this).scrollTop() > 0) {
-				$luz.addClass('active');
-				$luzTop.addClass('active');
-			}
-			if ($(this).scrollTop() > 20) {
-				$placaSombra.addClass('active');
-			}
-			if ($(this).scrollTop() < 20) {
-				$placaSombra.removeClass('active');
-			}
-			if ($(this).scrollTop() > heightScreen) {
-				$luzTop.removeClass('active');
-			}
-		}
 		if ($(this).scrollTop() > heightScreen - 50) {
 			$logo.addClass('active');
 		}
